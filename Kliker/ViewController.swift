f61453db9d7dd950fc44b541828e022e82c7b2d5
Admin <admin@Mac-Admin.local>
2014-12-24 22:43:39 +0300

    first commit

--- Kliker/ViewController.swift
+++ Kliker/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var i=0
+    var j=0
+    let max=5
+    var startTime=NSDate.timeIntervalSinceReferenceDate()
+    var bestTimeNow=0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+          // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +27,45 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func klikMe(sender: AnyObject)
+    {
+      //  self.speedLabel.text=String(startTime)
+        
+        if i==0{
+        startTime=NSDate.timeIntervalSinceReferenceDate()
+        }
+        i++
+        j++
+        self.klikLabel.text=String(j)
+        
+        if i==max
+        {
+           
+            var currentTime = NSDate.timeIntervalSinceReferenceDate()
+            var elapsedTime: NSTimeInterval = currentTime - startTime
+            self.speedLabel.text="Speed: " + String(format:"%.3f", elapsedTime)
+            i=0
+            
+            //First put
+            if j==max
+            {
+                self.bestTime.text="Best time now: " + String(format:"%.3f", elapsedTime)
+                bestTimeNow=elapsedTime
+            }
+            //Find best Time
+            if elapsedTime < bestTimeNow
+            {
+                bestTimeNow=elapsedTime
+                self.bestTime.text="Best time now: " + String(format:"%.3f", bestTimeNow)
+            }
+            
+            
+        }
+        
+    }
+    
+    @IBOutlet weak var bestTime: UILabel!
+    @IBOutlet weak var klikLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
 }
 
